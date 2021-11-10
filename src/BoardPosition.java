c69b293676b3eed004824e6f802fe0efd1fc3742
Daniel <dagohar246@gmail.com>
2021-11-10 22:50:51 +0100

    Initial Commit

---
+++ src/BoardPosition.java
@@ -0,0 +1,13 @@
+public class BoardPosition {
+    private char[][] fields;
+    
+    public void OnStart() {
+        fields = new char[8][8];
+
+        for(int i = 0; i < 8; i++) {
+            for(int p = 0; p < 8; p++) {
+                fields[i][p] = ' ';
+            }
+        }
+    }
+}
---
+++ src/ConsoleCommands.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class ConsoleCommands {
+    public static void ClearConsole() throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
+
+    public static void printBlack(char character) {
+        System.out.print("\u001B[90m" + character + "\u001B[0m");
+    }
+}
