cb73e460e43e77db5b440bca4a7258ca1364ad1b
Daniel <dagohar246@gmail.com>
2022-01-14 23:20:26 +0100

    Add square diagonal checking for MathExtended

--- src/com/company/MathExtended.java
+++ src/com/company/MathExtended.java
@@ -10,4 +10,16 @@ public class MathExtended {
 
         return Math.sqrt(Math.pow(vectorLength.getKey(), 2) + Math.pow(vectorLength.getValue(), 2));
     }
+
+    public static boolean IsSquareDiagonal(Pair<Integer, Integer> firstPoint, Pair<Integer, Integer> lastPoint) {
+        double length1, length2;
+
+        length1 = Magnitude(firstPoint, new Pair<>(firstPoint.getKey(), lastPoint.getValue()));
+        length2 = Magnitude(firstPoint, new Pair<>(lastPoint.getKey(), firstPoint.getValue()));
+
+        System.out.println(firstPoint.getKey() + ", " + firstPoint.getValue() + " : " + firstPoint.getKey() + ", " + lastPoint.getValue());
+        System.out.println(firstPoint.getKey() + ", " + firstPoint.getValue() + " : " + lastPoint.getKey() + ", " + firstPoint.getValue());
+
+        return length1 == length2;
+    }
 }
